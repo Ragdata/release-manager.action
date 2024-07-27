cffa371719e6ea5de279407826107a442d8ff221
Ragdata <ragdata@users.noreply.github.com>
2024-07-27 12:18:35 +1000

    refactor: Move functions to utils script

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,48 +18,8 @@ shopt -s inherit_errexit
 
 IFS=$'\n\t'	# set unofficial strict mode @see: http://redsymbol.net/articles/unofficial-bash-strict-mode/
 
-####################################################################
-# FUNCTIONS
-####################################################################
-re::errorHandler()
-{
-	local -n lineNo="${1:-LINENO}"
-	local -n bashLineNo="${2:-BASH_LINENO}"
-	local lastCommand="${3:-BASH_COMMAND}"
-	local code="${4:-0}"
+source scripts/utils.sh
 
-	local lastCommandHeight
-
-	[[ "$code" -eq 0 ]] && return 0
-
-	lastCommandHeight="$(wc -l <<<"${lastCommand}")"
-
-	local -a outputArray=()
-
-	outputArray+=(
-		'---'
-		"Line History: [${lineNo} ${bashLineNo[*]}]"
-		"Function Trace: [${FUNCNAME[*]}]"
-		"Exit Code: ${code}"
-	)
-
-	[[ "${#BASH_SOURCE[@]}" -gt 1 ]] && {
-		outputArray+=('source_trace:')
-		for item in "${BASH_SOURCE[@]}"; do
-			outputArray+=(" - ${item}")
-		done
-	} || outputArray+=("source_trace: [${BASH_SOURCE[*]}]")
-
-	[[ "${lastCommandHeight}" -gt 1 ]] && {
-		outputArray+=('last_command: ->' "${lastCommand}")
-	} || outputArray+=("last_command: ${lastCommand}")
-
-	outputArray+=('---')
-
-	printf '%s\n' "${outputArray[@]}" >&2
-
-	exit "$code"
-}
 ####################################################################
 # MAIN
 ####################################################################
